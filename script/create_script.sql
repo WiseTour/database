39add5bf026d00f664af31347a4a395d4537706a
sardinh4 <leonardo_sardinha@outlook.com>
2025-06-07 12:38:46 -0300

    Ajusta MER com configuração do Slack: apenas canal e estado de ativo; adiciona trigger no script

--- script/create_script.sql
+++ script/create_script.sql
@@ -1,13 +1,6 @@
-DROP DATABASE WiseTour;
 CREATE DATABASE IF NOT EXISTS WiseTour;
 
 USE WiseTour;
-
-CREATE TABLE unidade_federativa_brasil (
-sigla CHAR(2) PRIMARY KEY,
-unidade_federativa VARCHAR(45) NOT NULL UNIQUE,
-regiao VARCHAR(45)
-);
 
 /* ETL */
 CREATE TABLE origem_dados (
@@ -22,6 +15,41 @@ id_pais INT PRIMARY KEY AUTO_INCREMENT,
 pais VARCHAR(100) NOT NULL UNIQUE
 );
 
+CREATE TABLE unidade_federativa_brasil (
+sigla CHAR(2) PRIMARY KEY,
+unidade_federativa VARCHAR(45) NOT NULL UNIQUE,
+regiao VARCHAR(45)
+);
+
+INSERT INTO unidade_federativa_brasil (sigla, unidade_federativa, regiao) VALUES
+('AC', 'Acre', 'Norte'),
+('AL', 'Alagoas', 'Nordeste'),
+('AP', 'Amapá', 'Norte'),
+('AM', 'Amazonas', 'Norte'),
+('BA', 'Bahia', 'Nordeste'),
+('CE', 'Ceará', 'Nordeste'),
+('DF', 'Distrito Federal', 'Centro-Oeste'),
+('ES', 'Espírito Santo', 'Sudeste'),
+('GO', 'Goiás', 'Centro-Oeste'),
+('MA', 'Maranhão', 'Nordeste'),
+('MT', 'Mato Grosso', 'Centro-Oeste'),
+('MS', 'Mato Grosso do Sul', 'Centro-Oeste'),
+('MG', 'Minas Gerais', 'Sudeste'),
+('PA', 'Pará', 'Norte'),
+('PB', 'Paraíba', 'Nordeste'),
+('PR', 'Paraná', 'Sul'),
+('PE', 'Pernambuco', 'Nordeste'),
+('PI', 'Piauí', 'Nordeste'),
+('RJ', 'Rio de Janeiro', 'Sudeste'),
+('RN', 'Rio Grande do Norte', 'Nordeste'),
+('RS', 'Rio Grande do Sul', 'Sul'),
+('RO', 'Rondônia', 'Norte'),
+('RR', 'Roraima', 'Norte'),
+('SC', 'Santa Catarina', 'Sul'),
+('SP', 'São Paulo', 'Sudeste'),
+('SE', 'Sergipe', 'Nordeste'),
+('TO', 'Tocantins', 'Norte'),
+('OF', 'Outras Unidades da Federação', null);
 
 CREATE TABLE perfil_estimado_turistas (
 id_perfil_estimado_turistas INT AUTO_INCREMENT,
@@ -44,18 +72,6 @@ CONSTRAINT FOREIGN KEY (fk_uf_entrada) REFERENCES unidade_federativa_brasil (sig
 PRIMARY KEY (id_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada)
 );
 
-CREATE TABLE perfil_estimado_turista_origem (
-    fk_origem_dados INT,
-    fk_perfil_estimado_turistas INT,
-    fk_pais_origem INT,
-    fk_uf_entrada CHAR(2),
-    data_coleta DATE NOT NULL,
-    CONSTRAINT FOREIGN KEY (fk_origem_dados) REFERENCES origem_dados (id_origem_dados),
-    CONSTRAINT FOREIGN KEY (fk_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada)
-        REFERENCES perfil_estimado_turistas (id_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada),
-    PRIMARY KEY (fk_origem_dados, fk_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada)
-);
-
 CREATE TABLE destino (
 fk_perfil_estimado_turistas INT,
 fk_pais_origem INT,
@@ -69,6 +85,18 @@ CONSTRAINT FOREIGN KEY (fk_uf_destino) REFERENCES unidade_federativa_brasil (sig
 PRIMARY KEY (fk_perfil_estimado_turistas, fk_pais_origem, fk_uf_destino, fk_uf_entrada)
 );
 
+CREATE TABLE perfil_estimado_turista_origem (
+    fk_origem_dados INT,
+    fk_perfil_estimado_turistas INT,
+    fk_pais_origem INT,
+    fk_uf_entrada CHAR(2),
+    data_coleta DATE NOT NULL,
+    CONSTRAINT FOREIGN KEY (fk_origem_dados) REFERENCES origem_dados (id_origem_dados),
+    CONSTRAINT FOREIGN KEY (fk_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada)
+        REFERENCES perfil_estimado_turistas (id_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada),
+    PRIMARY KEY (fk_origem_dados, fk_perfil_estimado_turistas, fk_pais_origem, fk_uf_entrada)
+);
+
 /* LOG */
 
 CREATE TABLE etapa (
@@ -77,11 +105,26 @@ CREATE TABLE etapa (
     CONSTRAINT chk_etapa CHECK (etapa IN ('extracao', 'tratamento', 'transformacao', 'carregamento', 'conexao_s3', 'finalizacao', 'inicializacao'))
 );
 
+INSERT INTO etapa (id_etapa, etapa) VALUES
+(1, 'inicializacao'),
+(2, 'conexao_s3'),
+(3, 'extracao'),
+(4, 'tratamento'),
+(5, 'transformacao'),
+(6, 'carregamento'),
+(7, 'finalizacao');
+
 CREATE TABLE log_categoria (
 id_log_categoria INT PRIMARY KEY AUTO_INCREMENT,
 categoria VARCHAR(45) NOT NULL UNIQUE,
 CONSTRAINT chk_categoria CHECK (categoria IN('erro', 'aviso', 'sucesso', 'info'))
 );
+
+INSERT INTO log_categoria (id_log_categoria, categoria) VALUES
+(1, 'erro'),
+(2, 'aviso'),
+(3,'sucesso'),
+(4,'info');
 
 CREATE TABLE log (
 id_log INT AUTO_INCREMENT,
@@ -102,7 +145,6 @@ PRIMARY KEY (id_log, fk_log_categoria, fk_etapa)
 );
 
 /* USUÁRIO */
-
 CREATE TABLE usuario (
 id_usuario INT AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
@@ -112,58 +154,86 @@ CONSTRAINT chk_permissao CHECK (permissao IN ('admin', 'padrao', 'wisetour'))
 );
 
 /* CONFIGURACOES E PREFERÊNCIAS DASHBOARD E SLACK */
-
-CREATE TABLE configuracao_slack (
-id_configuracao_slack INT AUTO_INCREMENT,
-fk_usuario INT,
-slack_user_id VARCHAR(45) NULL,
-slack_username VARCHAR(255) NULL,
-canal_padrao VARCHAR(255) NULL,
-ativo CHAR(3) NOT NULL,
-CONSTRAINT chk_ativo CHECK (ativo IN ('sim', 'nao')),
-CONSTRAINT FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
-PRIMARY KEY (id_configuracao_slack, fk_usuario)
+CREATE TABLE tela_dashboard (
+id_tela_dashboard INT PRIMARY KEY AUTO_INCREMENT,
+tela VARCHAR(13) NOT NULL,
+CONSTRAINT chk_tela CHECK (tela IN ('sazonalidade', 'perfilTurista', 'panoramaGeral'))
 );
 
-CREATE TABLE tipo_notificacao_dados (
-fk_log_categoria INT,
-fk_configuracao_slack INT,
-fk_usuario INT,
-CONSTRAINT FOREIGN KEY (fk_log_categoria) REFERENCES log_categoria (id_log_categoria),
-CONSTRAINT FOREIGN KEY (fk_configuracao_slack, fk_usuario) REFERENCES configuracao_slack (id_configuracao_slack, fk_usuario),
-PRIMARY KEY (fk_log_categoria, fk_configuracao_slack, fk_usuario)
-);
+INSERT INTO tela_dashboard (tela) VALUES
+('sazonalidade'),
+('perfilTurista'),
+('panoramaGeral');
 
-CREATE TABLE preferencias_visualizacao_dashboard (
-id_preferencias_visualizacao_dashboard INT AUTO_INCREMENT,
-fk_usuario int,
+CREATE TABLE preferencia_visualizacao_dashboard (
+fk_tela_dashboard INT,
+fk_usuario INT,
 ativo CHAR(3) NOT NULL,
 CONSTRAINT chk_ativo_preferencias_visualizacao_dashboard CHECK (ativo IN ('sim', 'nao')),
 CONSTRAINT FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
-PRIMARY KEY (id_preferencias_visualizacao_dashboard, fk_usuario)
+CONSTRAINT FOREIGN KEY (fk_tela_dashboard) REFERENCES tela_dashboard (id_tela_dashboard),
+PRIMARY KEY (fk_tela_dashboard, fk_usuario)
 );
 
-CREATE TABLE tela_dashboard (
-id_tela_dashboard INT AUTO_INCREMENT,
-fk_preferencias_visualizacao_dashboard INT,
-fk_usuario INT,
-tela VARCHAR(13) NOT NULL,
-ativo CHAR(3) NOT NULL,
-CONSTRAINT chk_tela CHECK (tela IN ('sazonalidade', 'perfilTurista', 'panoramaGeral')),
-CONSTRAINT chk_ativo_tela_dashboard CHECK (ativo IN ('sim', 'nao')),
-CONSTRAINT FOREIGN KEY (fk_preferencias_visualizacao_dashboard, fk_usuario) REFERENCES preferencias_visualizacao_dashboard (id_preferencias_visualizacao_dashboard, fk_usuario),
-PRIMARY KEY (id_tela_dashboard, fk_preferencias_visualizacao_dashboard, fk_usuario)
+CREATE TABLE configuracao_slack (
+  id_configuracao_slack INT,
+  fk_usuario INT,
+  canal_padrao VARCHAR(255) NULL,
+  ativo CHAR(3) NOT NULL,
+  CONSTRAINT chk_ativo CHECK (ativo IN ('sim', 'nao')),
+  CONSTRAINT fk_config_usuario FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario),
+  PRIMARY KEY (id_configuracao_slack, fk_usuario)
 );
+
+CREATE TABLE tipo_notificacao_dados (
+fk_etapa INT,
+fk_configuracao_slack INT,
+fk_usuario INT,
+CONSTRAINT FOREIGN KEY (fk_etapa) REFERENCES etapa (id_etapa),
+CONSTRAINT FOREIGN KEY (fk_configuracao_slack, fk_usuario) REFERENCES configuracao_slack (id_configuracao_slack, fk_usuario),
+PRIMARY KEY (fk_etapa, fk_configuracao_slack, fk_usuario)
+);
+
+DELIMITER $$
+
+CREATE TRIGGER after_insert_usuario
+AFTER INSERT ON usuario
+FOR EACH ROW
+BEGIN
+  -- Verifica se a permissão do usuário NÃO é 'wisetour'
+  IF NEW.permissao <> 'wisetour' THEN
+
+    -- Inserir preferências de visualização do dashboard (todas como 'sim')
+    INSERT INTO preferencia_visualizacao_dashboard (fk_tela_dashboard, fk_usuario, ativo)
+    SELECT td.id_tela_dashboard, NEW.id_usuario, 'sim'
+    FROM tela_dashboard td;
+
+    -- Inserir configuração do Slack com id igual ao id do usuário
+    INSERT INTO configuracao_slack (id_configuracao_slack, fk_usuario, canal_padrao, ativo)
+    VALUES (NEW.id_usuario, NEW.id_usuario, NULL, 'nao');
+
+    -- Inserir tipos de notificações para cada etapa com a configuração do Slack recém-criada
+    INSERT INTO tipo_notificacao_dados (fk_etapa, fk_configuracao_slack, fk_usuario)
+    SELECT e.id_etapa, NEW.id_usuario, NEW.id_usuario
+    FROM etapa e;
+
+  END IF;
+END$$
+
+DELIMITER ;
+
+DELIMITER ;
+
 
 /* EMPRESA */
 
 CREATE TABLE informacao_contato_cadastro (
-id_informacao_contato_cadastro INT PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL,
-telefone VARCHAR(11) NOT NULL,
-nome VARCHAR(255) NOT NULL,
-fidelizado VARCHAR(255),
-CONSTRAINT chk_fidelizado CHECK (fidelizado IN ('sim', 'nao'))
+  id_informacao_contato_cadastro INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  telefone VARCHAR(11) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  fidelizado VARCHAR(255) DEFAULT 'nao',
+  CONSTRAINT chk_fidelizado CHECK (fidelizado IN ('sim', 'nao'))
 );
 
 CREATE TABLE historico_contato (
@@ -192,6 +262,8 @@ PRIMARY KEY (id_endereco, fk_uf_sigla)
 
 CREATE TABLE empresa (
 cnpj CHAR(14) UNIQUE,
+nome_fanatasia VARCHAR(255),
+razao_social VARCHAR(255),
 fk_informacao_contato_cadastro INT,
 fk_endereco INT,
 fk_uf_sigla CHAR(2),
--- script/insert_script.sql
+++ script/insert_script.sql
@@ -1,51 +1,6 @@
+/* SCRIPT PARA TESTES */
+
 USE WiseTour;
-
-INSERT INTO etapa (id_etapa, etapa) VALUES
-(1, 'inicializacao'),
-(2, 'conexao_s3'),
-(3, 'extracao'),
-(4, 'tratamento'),
-(5, 'transformacao'),
-(6, 'carregamento'),
-(7, 'finalizacao');
-
-INSERT INTO log_categoria (id_log_categoria, categoria) VALUES
-(1, 'erro'),
-(2, 'aviso'),
-(3,'sucesso'),
-(4,'info');
-
-INSERT INTO unidade_federativa_brasil (sigla, unidade_federativa, regiao) VALUES
-('AC', 'Acre', 'Norte'),
-('AL', 'Alagoas', 'Nordeste'),
-('AP', 'Amapá', 'Norte'),
-('AM', 'Amazonas', 'Norte'),
-('BA', 'Bahia', 'Nordeste'),
-('CE', 'Ceará', 'Nordeste'),
-('DF', 'Distrito Federal', 'Centro-Oeste'),
-('ES', 'Espírito Santo', 'Sudeste'),
-('GO', 'Goiás', 'Centro-Oeste'),
-('MA', 'Maranhão', 'Nordeste'),
-('MT', 'Mato Grosso', 'Centro-Oeste'),
-('MS', 'Mato Grosso do Sul', 'Centro-Oeste'),
-('MG', 'Minas Gerais', 'Sudeste'),
-('PA', 'Pará', 'Norte'),
-('PB', 'Paraíba', 'Nordeste'),
-('PR', 'Paraná', 'Sul'),
-('PE', 'Pernambuco', 'Nordeste'),
-('PI', 'Piauí', 'Nordeste'),
-('RJ', 'Rio de Janeiro', 'Sudeste'),
-('RN', 'Rio Grande do Norte', 'Nordeste'),
-('RS', 'Rio Grande do Sul', 'Sul'),
-('RO', 'Rondônia', 'Norte'),
-('RR', 'Roraima', 'Norte'),
-('SC', 'Santa Catarina', 'Sul'),
-('SP', 'São Paulo', 'Sudeste'),
-('SE', 'Sergipe', 'Nordeste'),
-('TO', 'Tocantins', 'Norte'),
-('OF', 'Outras Unidades da Federação', null);
-
-
 
 INSERT INTO informacao_contato_cadastro (email, telefone, nome, fidelizado)
 VALUES ('contato@wisetour.com.br', '11999999999', 'WiseTour Turismo Ltda.', 'sim');
@@ -57,67 +12,26 @@ INSERT INTO empresa (cnpj, fk_informacao_contato_cadastro, fk_endereco, fk_uf_si
 VALUES ('12345678000199', 1, 1, 'SP');
 
 INSERT INTO usuario (email, senha, permissao) VALUES
-('admin', 'admin123', 'admin'),
-('leonardo.sardinha@outlook.com', 'urubu100', 'padrao'),
+('admin', 'admin123', 'wisetour'),
+('leonardo.sardinha@outlook.com', 'urubu100', 'admin'),
 ('ian.medeiros@outlook.com', 'urubu100', 'padrao'),
-('luana.liriel@outlook.com', 'urubu100', 'padrao'),
+('luana.liriel@outlook.com', 'urubu100', 'admin'),
 ('kenner.lima@outlook.com', 'urubu100', 'padrao'),
-('phelipe.bruione@outlook.com', 'urubu100', 'padrao'),
+('phelipe.bruione@outlook.com', 'urubu100', 'admin'),
 ('samara.damaceno@outlook.com', 'urubu100', 'padrao');
 
 INSERT INTO funcionario (nome, cargo, telefone, fk_cnpj, fk_informacao_contato_cadastro, fk_uf_sigla, fk_endereco, fk_usuario) VALUES
-('Leonardo Sardinha', 'Analista de Dados', '11911111111', '12345678000199', 1, 'SP', 1, 2),
-('Ian Medeiros', 'Desenvolvedor Backend', '11922222222', '12345678000199', 1, 'SP', 1, 3),
-('Luana Liriel', 'Analista de Negócios', '11933333333', '12345678000199', 1, 'SP', 1, 4),
-('Kenner Lima', 'Dev Frontend', '11944444444', '12345678000199', 1, 'SP', 1, 5),
-('Phelipe Bruione', 'Product Owner', '11955555555', '12345678000199', 1, 'SP', 1, 6),
-('Samara Damaceno', 'UX Designer', '11966666666', '12345678000199', 1, 'SP', 1, 7);
+('Leonardo Sardinha', 'Desenvolvedor Back-end', '11911111111', '12345678000199', 1, 'SP', 1, 2),
+('Ian Medeiros', 'Product Owner', '11922222222', '12345678000199', 1, 'SP', 1, 3),
+('Luana Liriel', 'Desenvolvedor Front-end', '11933333333', '12345678000199', 1, 'SP', 1, 4),
+('Kenner Lima', 'Analista de Dados', '11944444444', '12345678000199', 1, 'SP', 1, 5),
+('Phelipe Bruione', 'Scrum Master', '11955555555', '12345678000199', 1, 'SP', 1, 6),
+('Samara Damaceno', 'Analista de Negócios', '11966666666', '12345678000199', 1, 'SP', 1, 7);
 
-INSERT INTO preferencias_visualizacao_dashboard (fk_usuario, ativo) VALUES
-(2, 'sim'),
-(3, 'sim'),
-(4, 'sim'),
-(5, 'sim'),
-(6, 'sim'),
-(7, 'sim');
+UPDATE configuracao_slack 
+SET canal_padrao = '#leonardo-alertas', ativo = 'sim' where fk_usuario = 2;
 
-
-INSERT INTO tela_dashboard (fk_preferencias_visualizacao_dashboard, fk_usuario, tela, ativo) VALUES
-
--- Usuário 2
-(1, 2, 'sazonalidade', 'sim'),
-(1, 2, 'perfilTurista', 'sim'),
-(1, 2, 'panoramaGeral', 'sim'),
-
--- Usuário 3
-(2, 3, 'sazonalidade', 'sim'),
-(2, 3, 'perfilTurista', 'sim'),
-(2, 3, 'panoramaGeral', 'sim'),
-
--- Usuário 4
-(3, 4, 'sazonalidade', 'sim'),
-(3, 4, 'perfilTurista', 'sim'),
-(3, 4, 'panoramaGeral', 'sim'),
-
--- Usuário 5
-(4, 5, 'sazonalidade', 'sim'),
-(4, 5, 'perfilTurista', 'sim'),
-(4, 5, 'panoramaGeral', 'sim'),
-
--- Usuário 6
-(5, 6, 'sazonalidade', 'sim'),
-(5, 6, 'perfilTurista', 'sim'),
-(5, 6, 'panoramaGeral', 'sim'),
-
--- Usuário 7
-(6, 7, 'sazonalidade', 'sim'),
-(6, 7, 'perfilTurista', 'sim'),
-(6, 7, 'panoramaGeral', 'sim');
-
-select * from perfil_estimado_turistas join pais on fk_pais_origem = id_pais where quantidade_turistas > 5 order by quantidade_turistas;
-SELECT SUM(quantidade_turistas) AS total_turistas
-FROM perfil_estimado_turistas;
-
-select * from log;
-
-
+select * from configuracao_slack;
+select * from tipo_notificacao_dados;
+select * from preferencia_visualizacao_dashboard order by fk_usuario;
+select * from informacao_contato_cadastro;
